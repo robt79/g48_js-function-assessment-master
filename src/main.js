c233387f0b3b2fc85309642de59e96308620b6cb
Robert Torres <robt792003@gmail.com>
2017-04-24 19:46:54 -0500

    updated

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@
 
 
 function question0() {
-  console.log('Hello JavaScript')
+  console.log('Hello JavaScript');
 }
+
+question0();
 
 
 
@@ -41,13 +43,16 @@ question2(7);
 // Write a function called `question3` that:
 // - Has two parameters
 // - Returns both arguments added together
-
-function question3(firstNum, secondNum) {
-  return firstNum + secondNum;
+function question3(param1, param2) {
+  return param1 + param2;
 
 }
 
-question3(1, 2);
+question3(1, 5);
+
+
+
+
 
 // Write a function called `question4` that:
 // - Has no parameters
@@ -66,36 +71,25 @@ question4();
 function checkWeekend(param1, param2) {
   switch (param1) {
     case 'Sat' || 'Sun':
-      console.log('Hooray! I can\'t wait to ' + param2)
+      console.log('Hooray! I can\'t wait to ' + param2);
       break;
     default:
-      console.log('Better get back to work!')
+      console.log('Better get back to work!');
   }
 }
-var anything1 = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+var anything1 = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
 var anything2 = 'Music';
-var Party = "Drugs";
+var Party = "Have a beer!";
 
 function question5(days, weekendActivity) {
 
   for (var i = 0; i < days.length; i++) {
-    checkWeekend(days[i], weekendActivity)
-
-
-
-    //   switch (days[i]) {
-    //     case 'Sat' || 'Sun':
-    //     console.log('Hooray! I can\'t wait to ' + weekendActivity)
-    //     break;
-    //     default:
-    //     console.log('Better get back to work!')
-    //   }
+    checkWeekend(days[i], weekendActivity);
   }
-
-
-  return weekendActivity
+  return weekendActivity;
 }
 question5(anything1, Party);
+
 module.exports = {
   question0,
   question1,
